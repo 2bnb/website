6b62e4a18e6a7ec7747369ef1068b7ea11181e69
Samantha <samantha@focalstrategy.com>
2020-01-16 00:30:10 +0000

    Update Model and Migration to Match Current docs

--- app/User.php
+++ app/User.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Passport\HasApiTokens;
@@ -10,67 +11,75 @@ use OwenIt\Auditing\Contracts\Auditable;
 
 class User extends Authenticatable implements Auditable
 {
-	use HasApiTokens, Notifiable, \OwenIt\Auditing\Auditable;
+    use HasApiTokens, Notifiable, \OwenIt\Auditing\Auditable, SoftDeletes;
 
-	/**
-	 * The attributes that are mass assignable.
-	 *
-	 * @var array
-	 */
-	protected $fillable = [
-		'name', 'join_date', 'discord_id', 'discord_access_token',
-		'avatar_resource_id', 'date_of_birth', 'country', 'timezone',
-		'data', 'status_id', 'rank_id', 'qualification_id',
-	];
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'join_date',
+        'discord_id',
+        'discord_access_token',
+        'avatar_resource_id',
+        'date_of_birth',
+        'country',
+        'timezone',
+        'data',
+        'rank_id',
+        'qualification_id',
+    ];
 
-	/**
-	 * The attributes that should be hidden for arrays.
-	 *
-	 * @var array
-	 */
-	protected $hidden = [
-		'discord_access_token',
-	];
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'discord_access_token',
+    ];
 
-	/**
-	 * The attributes that should be cast to native types.
-	 *
-	 * @var array
-	 */
-	protected $casts = [
-		'data' => 'array',
-	];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'data' => 'array',
+    ];
 
-	/**
-	 * Don't allow incrememnting since we're using uuids
-	 *
-	 * @var boolean
-	 */
-	public $incrementing = false;
+    /**
+     * Don't allow incrememnting since we're using uuids
+     *
+     * @var boolean
+     */
+    public $incrementing = false;
 
-	/**
-	 * The primary key column name
-	 *
-	 * @var  string
-	 */
-	protected $primaryKey = 'uuid';
+    /**
+     * The primary key column name
+     *
+     * @var  string
+     */
+    protected $primaryKey = 'uuid';
 
-	/**
-	 * Auto-generate and fill the uuid field
-	 */
-	protected static function boot()
-	{
-		parent::boot();
-		static::creating(function ($user) {
-			$user->{$user->getKeyName()} = (string) Str::uuid();
-		});
-	}
+    /**
+     * Auto-generate and fill the uuid field
+     */
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($user) {
+            $user->{$user->getKeyName()} = (string) Str::uuid();
+        });
+    }
 
-	/**
-	 * Force keytype to string since it's a uuid not an id
-	 */
-	public function getKeyType()
-	{
-		return 'string';
-	}
+    /**
+     * Force keytype to string since it's a uuid not an id
+     */
+    public function getKeyType()
+    {
+        return 'string';
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -7,37 +7,37 @@ use Illuminate\Database\Query\Expression;
 
 class CreateUsersTable extends Migration
 {
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('users', function (Blueprint $table) {
-			$table->uuid('uuid')->primary();
-			$table->string('name');
-			$table->date('join_date')->nullable();
-			$table->string('discord_id')->unique();
-			$table->string('discord_access_token')->nullable();
-			$table->bigInteger('avatar_resource_id')->unsigned();
-			$table->date('date_of_birth');
-			$table->smallInteger('country')->unsigned()->zerofill()->nullable();
-			$table->string('timezone', 9)->default('UTC+00:00');
-			$table->json('data')->default(new Expression('(JSON_ARRAY())'));
-			$table->tinyInteger('status_id')->unsigned();
-			$table->smallInteger('rank_id')->unsigned();
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid('uuid')->primary();
+            $table->string('name');
+            $table->date('join_date')->nullable();
+            $table->string('discord_id')->unique();
+            $table->string('discord_access_token')->nullable();
+            $table->bigInteger('avatar_resource_id')->unsigned()->nullable();
+            $table->date('date_of_birth');
+            $table->smallInteger('country')->unsigned()->zerofill()->nullable()->default('Earth');
+            $table->string('timezone', 9)->default('UTC+00:00');
+            $table->json('data')->default(new Expression('(JSON_ARRAY())'));
+            $table->smallInteger('rank_id')->unsigned()->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::dropIfExists('users');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
 }
