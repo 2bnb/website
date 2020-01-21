965506c66e66d1b57cf623e2f37281bd87349e8b
Saborknight <support@saborknight.com>
2020-01-22 10:33:53 +1100

    Fixed - Spaces are back to tabs
    Issue ID: N/A

--- database/factories/ConfigFactory.php
+++ database/factories/ConfigFactory.php
@@ -6,31 +6,32 @@ use App\Config;
 use Faker\Generator as Faker;
 
 $factory->define(Config::class, function (Faker $faker) {
-    return [
-    	'name' => $faker->randomElement([
-    		'social_media_links',
-    		'website_name',
-    		'website_title',
-    		'email_links',
-    		'unit_tag',
-    		'global_currency',
-    		'award_types',
-    		'resource_types',
-    		'event_types',
-    		'post_types'
-    	])
-    ];
+	return [
+		'name' => $faker->randomElement([
+			'social_media_links',
+			'website_name',
+			'website_title',
+			'email_links',
+			'unit_tag',
+			'global_currency',
+			'default_avatar',
+			'award_types',
+			'resource_types',
+			'event_types',
+			'post_types'
+		])
+	];
 });
 
 $factory->state(Config::class, 'values', function (Faker $faker) {
-    return [
-    	'value' => $faker->randomElement([
-    		'https://www.youtube.com/watch?v=eaW76aDKObk&list=PLA_zjX3swAf5tsw9KdHOGodzcJ_PptL9a',
-    		'2nd Battalion, Nord Brigade',
-    		'{"Qualification","Award"}',
-    		'EUR',
-    		'{"Operation","Training","Mini-op"}',
-    		'{"Blog","Annoucement"}'
-    	])
-    ];
+	return [
+		'value' => $faker->randomElement([
+			'https://www.youtube.com/watch?v=eaW76aDKObk&list=PLA_zjX3swAf5tsw9KdHOGodzcJ_PptL9a',
+			'2nd Battalion, Nord Brigade',
+			'{"Qualification","Award"}',
+			'EUR',
+			'{"Operation","Training","Mini-op"}',
+			'{"Blog","Annoucement"}'
+		])
+	];
 });
