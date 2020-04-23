cb23073713b0612d72657469ddf4ed79baf78e9f
Samantha <samantha@focalstrategy.com>
2020-04-23 10:02:56 +0100

    Roles test updgrade

--- tests/Feature/RolesTest.php
+++ tests/Feature/RolesTest.php
@@ -15,9 +15,26 @@ class RolesTest extends TestCase
 	/** @test */
 	public function can_interact_with_database() {
 		$roles = factory(Role::class, 3)->create();
+		$testRole = $roles->first();
+		$originalValues = $testRole->toArray();
 
 		$this->assertDatabaseHas('roles', [
 			'name' => $roles->first()['name']
+		]);
+
+		$testRole->name = 'TEST-ROLE';
+		$testRole->save();
+
+		$this->assertDatabaseMissing('roles', $originalValues);
+
+		$this->assertDatabaseHas('roles', [
+			'name' => 'TEST-ROLE'
+		]);
+
+		$testRole->delete();
+
+		$this->assertSoftDeleted('roles', [
+			'name' => $testRole->name
 		]);
 	}
 }
